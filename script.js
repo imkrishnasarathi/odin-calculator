e2d4c9646f0f51b7ce606510f7d40a96671c85ee
Krishna <krishnasarathishem@gmail.com>
2023-04-25 18:52:46 +0530

    Add function for multiplication in JavaScript file

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-function add(...n){
-  const sum = n.reduce((sum, number)=>{return sum+number},0);
-  return sum;
+function add(...n) {
+	const sum = n.reduce((sum, number) => {
+		return sum + number;
+	}, 0);
+	return sum;
 }
 
-function subtract(n1,n2){
-    return n1 - n2
+function subtract(n1, n2) {
+	return n1 - n2;
+}
+
+function multiply(...n) {
+	const product = n.reduce((product, number) => {
+		return product * number;
+	}, 1);
+    return product;
 }
