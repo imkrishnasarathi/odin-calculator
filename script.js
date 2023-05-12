c2e25dbdada291070770f298f1b2733e4d13dae7
Krishna <krishnasarathishem@gmail.com>
2023-05-12 19:02:43 +0530

    Add keyboard support, fix bugs and organise

--- script.js
+++ script.js
@@ -11,7 +11,112 @@ let dot = document.querySelector('#dot');
 let inverse = document.querySelector('#inverse')
 let percent = document.querySelector('#percent')
 
+function evaluate(){
+  if(operator!== null && !shouldResetDisplay){
+    calculate();
+  }
+  operator = null;
+}
 
+window.addEventListener('keydown', (e)=>{
+  if(e.key>=0 && e.key<=9){
+    if (shouldResetDisplay){
+      displayVal = (e.key).toString();
+      shouldResetDisplay = false;
+    }
+    else{
+      if(displayVal === '0'){
+        displayVal = (e.key).toString();
+      }
+      else{
+        displayVal+=(e.key).toString();
+      }
+    }
+    display.textContent = displayVal;
+  }
+  if(e.key === '.'){
+    if(periodEntered!==true){
+      if (shouldResetDisplay || displayVal==='0'){
+        displayVal = '0.';
+        periodEntered = true;
+        shouldResetDisplay = false;
+      }
+      else{
+        periodEntered = true;
+        displayVal+='.';
+      }
+    }
+    else{
+      return;
+    }
+    display.textContent = displayVal;
+  }
+  if(e.key === '=' || e.key === 'Enter'){
+    evaluate();
+  }
+  if (e.key === 'Backspace') {
+    displayVal = displayVal.substring(0, displayVal.length-1)
+    if (displayVal==='' || displayVal===' '){
+      display.textContent = '0';
+    }
+    else{
+      display.textContent = displayVal;
+    }
+  }
+  if (e.key === 'Escape'){
+    clearCalculator();
+  }
+  if (e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/'){
+    let symbol = convertOperator(e.key)
+    periodEntered = false;
+    if (operator !== null && !shouldResetDisplay) {
+      calculate();
+    }
+    operator = symbol;
+    n1 = parseFloat(displayVal);
+    shouldResetDisplay = true;
+
+    symbols.forEach(btn => {
+      btn.classList.remove('operator');
+    });
+    switch (symbol){
+      case '+':
+        document.querySelector('#plus').classList.add('operator');
+        break;
+      case '-':
+        document.querySelector('#minus').classList.add('operator');
+        break;
+      case '×':
+        document.querySelector('#multiply').classList.add('operator');
+        break;
+      case '÷':
+        document.querySelector('#divide').classList.add('operator');
+        break;
+        }
+  }
+  if (e.key === 'F9'){
+    inverseVal = parseFloat(displayVal) - (parseFloat(displayVal) * 2);
+    displayVal = inverseVal.toString();
+    display.textContent = displayVal
+  }
+  if (e.key === '%'){
+    n1 = displayVal;
+    const n2 = 100;
+    let result = null;
+    result = operate(n1,3,n2)
+    if ((result % 1) !==0){
+      if (countDecimals(parseFloat(result)) > 7){
+        result = parseFloat(result).toFixed(7);
+      } else{
+        result = parseFloat(result).toFixed(countDecimals(parseFloat(result)));
+      }
+    }
+    displayVal = parseFloat(result).toString();
+    display.textContent = displayVal;
+    n1 = result;
+    shouldResetDisplay = true;
+  }
+})
 
 function clearCalculator() {
   displayVal = '0';
@@ -21,6 +126,18 @@ function clearCalculator() {
   n2 = null;
   shouldResetDisplay = false;
   periodEntered = false;
+}
+
+function convertOperator(keyboardOperator){
+  if (keyboardOperator === '/'){
+    return '÷';
+  }
+  else if (keyboardOperator === '*'){
+    return '×';
+  }
+  else{
+    return keyboardOperator;
+  }
 }
 
 function add(n1, n2) {
@@ -81,9 +198,13 @@ symbols.forEach(symbol => {
   });
 });
 
-let countDecimals = function (value) {
-  if(Math.floor(value) === value) return 0;
-  return value.toString().split(".")[1].length || 0;
+function countDecimals(number) {
+  if (typeof number !== 'undefined' && number !== null) {
+    var string = number.toString();
+    var index = string.indexOf('.');
+    return index === -1 ? 0 : string.length - index - 1;
+  }
+  return 0;
 }
 
 function calculate(){
@@ -114,13 +235,17 @@ function calculate(){
       result = parseFloat(result).toFixed(countDecimals(parseFloat(result)));
     }
   }
-  if (result === 'Infinity' || result === Infinity){
+  if (result === 'Infinity' || result === Infinity || result === NaN){
     result = 'Cannot divide by 0';
+    displayVal = result.toString();
+    display.textContent = displayVal.toString();
   }
-  displayVal = parseFloat(result).toString();
-  display.textContent = displayVal;
-  n1 = result;
-  shouldResetDisplay = true;
+  else{
+    displayVal = parseFloat(result).toString();
+    display.textContent = displayVal;
+    n1 = result;
+    shouldResetDisplay = true;
+  }
 
   // Remove the 'operator' class from all operator buttons
   symbols.forEach(btn => {
@@ -160,19 +285,28 @@ dot.addEventListener('click', (e)=>{
   display.textContent = displayVal;
 })
 
-percent.addEventListener("click", function(e){
-  e.preventDefault();
-  percentageVal = parseFloat(displayVal)/100;
-  displayVal = percentageVal.toString();
-  display.textContent = displayVal;
-})
-
-equal.addEventListener('click', function(){
-  if(operator!== null && !shouldResetDisplay){
-    calculate();
+percent.addEventListener("click", function(e) {
+  n1 = displayVal;
+  const n2 = 100;
+  let result = null;
+  result = operate(n1,3,n2)
+  if ((result % 1) !==0){
+    if (countDecimals(parseFloat(result)) > 7){
+      result = parseFloat(result).toFixed(7);
+    } else{
+      result = parseFloat(result).toFixed(countDecimals(parseFloat(result)));
+    }
   }
-  operator = null;
-})
+  displayVal = parseFloat(result).toString();
+  display.textContent = displayVal;
+  n1 = result;
+  shouldResetDisplay = true;
+});
+
+
+
+equal.addEventListener('click', evaluate)
+
 
 function operate(n1, operator, n2){
   if (operator===0){
@@ -195,3 +329,4 @@ function operate(n1, operator, n2){
     return;
   }
 }
+
